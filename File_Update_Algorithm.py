2900bc16d8f8f98a004a60901dcb6c8759448b05
CyberAnt-Coder <A.Tran9351@gmail.com>
2024-09-18 16:37:41 -0700

    Add files via upload

---
+++ File_Update_Algorithm.py
@@ -0,0 +1,25 @@
+# Update file through Python
+
+# assign 'import file' to the name of the file
+import_file = "allow_list.txt"
+
+# create 'with' statement to open the file
+with open(import_file, "r") as file:
+  
+  #use .read() to convert the file into a string 
+  ip_addresses = file.read()
+  
+# convert the string into a list
+ip_addresses = ip_addresses.split()
+
+# build for loop
+#if element is in 'remove_list', remove it
+for element in remove_list:
+  if element in ip_addresses:
+    ip_addresses.remove(element)
+
+#convert 'ip_addresses' back into a string
+ip_addresses = "\n".join(ip_addresses)
+#create 'with' statement to rewrite the original file
+with open(import_file, "w") as file:
+  file.write(ip_addresses)
